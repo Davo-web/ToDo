d4d4087715192a7cc3cfdd9b036f983ed5bf22f5
David <goodostalm@gmail.com>
2026-02-23 21:29:09 +0300

    edit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', function(e) {
         </label>
         <p class="task">${taskText}</p>
         <button class="edit-btn">
-            <img src="./Vector.png" alt="edit" class="edit-img">
+            <img src="./img/Vector.png" alt="edit" class="edit-img">
         </button>
         <button class="delete-btn">
             <span class="line1"></span>
@@ -26,34 +26,86 @@ form.addEventListener('submit', function(e) {
     `
     ToDo.insertAdjacentHTML('afterend', taskCardHtml); // вставляем задачу после контейнера
 
-    let taskCard = document.querySelector('.task-card');
-    let deleteBtn = document.querySelector('.delete-btn');
-    if (document.querySelector('.task-card')) { // проверяем есть ли на сайте задача
-        message.textContent = 'Работаем!';
-        deleteBtn.addEventListener('click', function() { // удаление задач
-            taskCard.remove();
-            if (!document.querySelector('.task-card')) message.textContent = 'Задач нет. Отдыхай!';
-        })
-    }
-
-    // зачёркивание задачи при нажатом checkbox
     let task = document.querySelector('.task');
     let checkbox = document.querySelector('.checkbox');
-    checkbox.addEventListener('change', function(){
-        if (checkbox.checked) task.classList.add('active');
-        else task.classList.remove('active');
-
-        function allChecked() {
+    function allChecked() { // проверка если выделены все чекбоксы
             let checkboxes = document.querySelectorAll('.checkbox');
-            return checkboxes.length > 0 && 
+            return checkboxes.length > 0 &&  // если чекбоксов не 0 и выбранных = количество чекбоксов
                 document.querySelectorAll('.checkbox:checked').length === checkboxes.length;
         }
+    checkbox.addEventListener('change', function(){
+        if (checkbox.checked) task.classList.add('active'); // перечё1ркивание
+        else task.classList.remove('active');
+
+        
         if (allChecked()) message.textContent = 'Задачи сделаны. Отдыхай!';
         if (!allChecked()) message.textContent = 'Работаем!';
     }) 
 
 
 
+
+    let taskCard = document.querySelector('.task-card');
+    let deleteBtn = document.querySelector('.delete-btn');
+    if (document.querySelector('.task-card')) { // проверяем есть ли на сайте задача
+        message.textContent = 'Работаем!';
+        deleteBtn.addEventListener('click', function() { // удаление задач
+            taskCard.remove();
+            if (allChecked()) message.textContent = 'Задачи сделаны. Отдыхай!';
+            if (!allChecked()) message.textContent = 'Работаем!';
+            if (!document.querySelector('.task-card')) message.textContent = 'Задач нет. Отдыхай!';
+        })
+    }
+
+
+
+
+    let editBtn = document.querySelector('.edit-btn');
+    editBtn.addEventListener('click', function() {
+        // вставляем поле ввода вместо текста задачи
+        let editInputHtml = `
+        <input type="text" value="${task.textContent}" class="edit-input">
+        `
+        task.insertAdjacentHTML('afterend', editInputHtml);
+        task.remove(); 
+
+        // фокус на инпут, курсор в конец текста
+        let editInput = document.querySelector('.edit-input');
+        editInput.focus();
+        let editInputValue = editInput.value; // запоминаем текст
+        editInput.value = ""; // сбрасываем инпут, чтоб курсор был в конце текста
+        editInput.value = editInputValue; // возвращаем значение
+
+
+
+        // кнопка подтверждения изменений вместо карандаша
+        let edit2 = `
+        <button class="edit-btn-ok">
+            <img src="./img/ok.svg" alt="ok" class="edit-img">
+        </button>
+        `
+        editBtn.insertAdjacentHTML('afterend', edit2);
+        editBtn.remove(); 
+
+
+        editInput.addEventListener('blur', function() {
+            taskText = editInput.value;
+            task = `
+            <p class="task">${taskText}</p>
+            `
+            editInput.insertAdjacentHTML('afterend', task);
+            editInput.remove();
+            editBtn = `
+            <button class="edit-btn">
+                <img src="./img/Vector.png" alt="edit" class="edit-img">
+            </button>
+            `
+            console.log(editBtn);
+            edit2 = document.querySelector('.edit-btn-ok');
+            edit2.insertAdjacentHTML('afterend', editBtn);
+            edit2.remove();
+        })
+    })
 
 
     input.value = '';
